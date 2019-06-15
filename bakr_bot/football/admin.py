da6263311a381d015e90782ad482e837d9c65c95
Mohamed Taha <mohamedtaha3333@gmail.com>
2019-06-15 17:44:29 +0200

    Rename models fields

--- bakr_bot/football/admin.py
+++ bakr_bot/football/admin.py
@@ -2,21 +2,21 @@ from django.contrib import admin
 from bakr_bot.football.models import Country, League, Team
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'livescore_id', 'name', 'is_real']
+    list_display = ['id', 'api_id', 'name', 'is_real']
 
 
 admin.site.register(Country, CountryAdmin)
 
 
 class LeagueAdmin(admin.ModelAdmin):
-    list_display = ['id', 'livescore_id', 'name', 'country']
+    list_display = ['id', 'api_id', 'name', 'country']
 
 
 admin.site.register(League, LeagueAdmin)
 
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ['id', 'livescore_id', 'name', 'league']
+    list_display = ['id', 'api_id', 'name', 'league']
 
 
 admin.site.register(Team, TeamAdmin)
--- bakr_bot/football/models.py
+++ bakr_bot/football/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.db.models.fields import BooleanField
+from django.utils.translation import ugettext_lazy as _
 from model_utils.models import TimeStampedModel
 
 
 class Country(TimeStampedModel):
-    livescore_id = models.CharField(max_length=250, unique=True)  # ID on the API provider (livescore-api.com)
+    api_id = models.CharField(max_length=250, unique=True)  # ID on the API provider
     name = models.CharField(max_length=150)
     is_real = models.CharField(max_length=1, default='1')
 
@@ -13,9 +15,9 @@ class Country(TimeStampedModel):
 
 class League(TimeStampedModel):
     country = models.ForeignKey(Country, related_name='leagues', on_delete=models.CASCADE)
-    livescore_id = models.CharField(max_length=250, unique=True)  # ID on the API provider (livescore-api.com)
+    api_id = models.CharField(max_length=250, unique=True)  # ID on the API provider
     name = models.CharField(max_length=250)
-    livescore_country_id = models.CharField(max_length=250)
+    is_supported = models.BooleanField(_('Is league supported by KoraInbox?'), default=False)
 
     def __str__(self):
         return "%s - %s" % (self.name, self.country.name)
@@ -23,9 +25,8 @@ class League(TimeStampedModel):
 
 class Team(TimeStampedModel):
     league = models.ForeignKey(League, related_name='teams', on_delete=models.CASCADE)
-    livescore_id = models.CharField(max_length=250, unique=True)
+    api_id = models.CharField(max_length=250, unique=True)
     name = models.CharField(max_length=250)
-    livescore_league_id = models.CharField(max_length=250)
 
     def __str__(self):
         return "%s - %s" % (self.name, self.league.country.name)
