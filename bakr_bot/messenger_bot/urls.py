e56cff55e8e1dc053328e0209413a2bf328d99f8
Mohamed Taha <mohamedtaha3333@gmail.com>
2019-09-17 16:37:30 +0200

    Comment

--- bakr_bot/messenger_bot/urls.py
+++ bakr_bot/messenger_bot/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib.auth.urls import urlpatterns
 
-from bakr_bot.messenger_bot.views import MessengerBotView
+# from bakr_bot.messenger_bot.views import MessengerBotView
 
-urlpatterns = [
-    url('^88c7f15d7842910c2dbc7b3822d9ed147da391f7d1a180b47c/?$', MessengerBotView.as_view())
-]
+# urlpatterns = [
+#     url('^88c7f15d7842910c2dbc7b3822d9ed147da391f7d1a180b47c/?$', MessengerBotView.as_view())
+# ]
