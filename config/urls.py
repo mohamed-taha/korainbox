ce361a4a3b362c8caa41e9a327f3dcee02916f38
Mohamed Taha <mohamedtaha3333@gmail.com>
2019-10-08 16:19:32 +0200

    Add Arabic privacy page and enhance homepage

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
-        "privacy-policy/", TemplateView.as_view(template_name="pages/privacy.html"), name="privacy-policy"
+        "privacy-policy/", TemplateView.as_view(template_name="pages/privacy_ar.html"), name="privacy-policy"
+    ),
+    path(
+        "privacy-policy-en/", TemplateView.as_view(template_name="pages/privacy.html"), name="privacy-policy-en"
     ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
